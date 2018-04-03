39cf6e986f18420022ef281fa804849f6ea84148
Wataru Sakurai <sakura11054@gmail.com>
2018-04-03 19:21:52 +0900

    commund create now

--- scripts/rec_move.py
+++ scripts/rec_move.py
@@ -2,20 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-import re
+import array
 import serial
+
 from std_msgs.msg import Int32
 
+#シリアル通信の設定
+ser = serial.Serial(
+    port = 0,
+    boundrate = 9600,
+    bytesize = 8,
+    parity = None,
+    stopbits = 1,
+    timeout = 0
+    )
+print ser.portstr
+
+
+
+
 def callback(msg):
-    #COMポートの設定
-    ser = serial.Serial(0,9600,serial.PARITY_NONE,serial.EIGHTBITS,serial.STOPBITS_ONE,1)
-    print ser.portstr
-    ser.write
-    print msg.dat
+    byte data[8]
+    data[0] = byte(0xAF)
 
 rospy.init_node('subscriber')
 
 sub = rospy.Subscriber('recognition',Int32,callback)
 
 rospy.spin()
+
 
