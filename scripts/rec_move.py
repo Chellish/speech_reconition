207adc4e45ad53a8f0e3ea55a6cf230cbacdd851
Wataru Sakurai <sakura11054@gmail.com>
2018-04-05 18:44:54 +0900

    make setJoy com and delete setSpeed com

--- scripts/rec_move.py
+++ scripts/rec_move.py
@@ -8,10 +8,11 @@ import numpy as np
 from std_msgs.msg import Int32
 from struct import pack
 
-com_Setpower = pack('5B',0xAF,0x03,0x02,0x01,0xB5)
+setPower_com = pack('5B',0xAF,0x03,0x02,0x01,0xB5)
 
 flag = 0
 setSpeed_com = []
+setJoy_com = [0xAF,0x05,0x03,0x00,0,0,0]
 
 #シリアル通信の設定
 ser = serial.Serial(
@@ -26,69 +27,62 @@ print ser.portstr
 
 
 class send_command(object):
-    #電源ONのコマンドを送る
-    def setPower(self):
-        ser.write(com_Setpower)
-        global flag
-        flag += 1
-
     #Checksumの計算
     #今回はXORを用いる
-    def checksum(self):
-        global setSpeed_com
+    def checksum(self,buf):
         csum = 0
-        for i in range(len(setSpeed_com)):
-            csum = csum ^ setSpeed_com[i]
+        for i in range(len(buf)-1):
+            csum = csum ^buf[i]
         temp = csum
         if (temp ^ csum) == 0:
-            setSpeed_com += [csum]
-            return True
+            return csum
         else:  
             print ("Error CheckSum!!")
-            return False
+            return 
 
     
     #速度制御に関するコマンド
-    def setSpeed(self,FM1,FA1,FD1,RM1,RA1,RD1,TM1,TA1,TD1):
-        global setSpeed_com
-        setSpeed_com += [0xAF,0x0C,0x04,FM1,FA1,FD1,RM1,RA1,RD1,TM1,TA1,TD1]
+    #def setSpeed(self,FM1,FA1,FD1,RM1,RA1,RD1,TM1,TA1,TD1):
+    #    global setSpeed_com
+    #    setSpeed_com += [0xAF,0x0C,0x04,FM1,FA1,FD1,RM1,RA1,RD1,TM1,TA1,TD1]
 
+    def setJoy(self,fb_para,lr_para):
+        global setJoy_com
+        setJoy_com[4] = fb_para
+        setJoy_com[5] = lr_para       
+        setJoy_com[6] = self.checksum(setJoy_com)
 
     def run(self,msg):
         rospy.init_node('subscriber')
+        #flagが0なら電源ONのコマンドを送る
         if flag == 0:
-            self.setPower()
+            global flag
+            flag += 1
+            command = setPower_com
+        #停止
         if msg.data == 0:
-            setSpeed_com(
-                F_M1=0x08,
-                F_A1=0x01,
-                F_D1=0x01,
-                R_M1=0x08,
-                R_A1=0x01,
-                R_D1=0x01,
-                T_M1=0x08,
-                T_A1=0x01,
-                T_D1=0x01)
-            command = pack("{0:d}B",*setSpeed_com)
-            check_sum = self.checksum()
-            if check_sum == True:
-                ser.write(command)
-
+            #joy_paramater: fb is max 0x64, min  
+            self.setJoy(0x00,0x00)
+            command = pack("{0:d}B",*setJoy_com)
+        #前進
         if msg.data == 1:
-            setSpeed_com(
-                F_M1=0x1E,
-                F_A1=0x0A,
-                F_D1=0x01,
-                R_M1=0x08,
-                R_A1=0x01,
-                R_D1=0x01,
-                T_M1=0x08,
-                T_A1=0x01,
-                T_D1=0x01)
-            command = pack("{0:d}B",*setSpeed_com)
-            check_sum = self.checksum()
-            if check_sum == True:
-                ser.write(command)
+            self.setJoy( 0x32,0x00)
+            command = pack("{0:d}B",*setJoy_com)
+        #後退
+        if msg.data == 2:
+            #パラメーターの負の値ってどうすんだろ？？
+            self.setJoy(0x00,0x00)
+            command = pack("{0:d}B",*setJoy_com)
+        #右
+        if msg.data == 3:
+            self.setJoy(0x32,0x00)
+            #パラメーターの負の値ってどうすんだろ？？
+            command = pack("{0:d}B",*setJoy_com)
+        #左
+        if msg.data == 4:
+            self.setJoy(0x00,0x32)
+            command = pack("{0:d}B",*setJoy_com)
+        ser.write(command)    
         rospy.spin()
 
 if __name__ == '__main__':
